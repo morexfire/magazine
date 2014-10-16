42350ca822e60c2dccc51a5f6a6fdcddd3765c3e
Shane Becker <veganstraightedge@gmail.com>
2014-10-16 16:00:40 -0700

    Fix homepage tile resizing for second row of tiles too

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -14,19 +14,19 @@
     });
 
     if(heights[0][0] < heights[0][1]) {
-      $(".h-entry", $rows[0]).css("height", heights[0][1])
+      $(".h-entry", $rows[0]).css("height", heights[0][1]);
     }
 
     if(heights[0][0] > heights[0][1]) {
-      $(".h-entry", $rows[0]).css("height", heights[0][1])
+      $(".h-entry", $rows[0]).css("height", heights[1][0]);
     }
 
     if(heights[1][0] < heights[1][1]) {
-      $(".h-entry", $rows[0]).css("height", heights[0][1])
+      $(".h-entry", $rows[1]).css("height", heights[1][1]);
     }
 
     if(heights[1][0] > heights[1][1]) {
-      $(".h-entry", $rows[0]).css("height", heights[0][1])
+      $(".h-entry", $rows[1]).css("height", heights[1][0]);
     }
 
   });
