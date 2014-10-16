5fe5914f50435385403c51861ef51047956e6ddc
Shane Becker <veganstraightedge@gmail.com>
2014-10-15 17:18:38 -0700

    Add additional layout meta and empty helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def rel_canonical_link_tag(page=nil)
+  end
+
+  def rel_og_image_tag(page=nil)
+  end
+
+  def rel_canonical_link_tag(page=nil)
+  end
+
+  def page_title(page=nil)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   resources :articles, :roles, :people
 
+  # atom feed
+  get "/feed", to: "articles#index", defaults: { format: "atom" }, as: :feed
+
   root "welcome#index"
 
+  get "(/:year)(/:month)(/:day)", to: "articles#index"
 
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # namespace :admin do
+  #   # Directs /admin/products/* to Admin::ProductsController
+  #   # (app/controllers/admin/products_controller.rb)
+  #   resources :products
+  # end
 end
