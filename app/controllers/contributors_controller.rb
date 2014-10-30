f4457255d9964b4b38b458d0b3d330ff54ba003c
Shane Becker <veganstraightedge@gmail.com>
2014-10-29 20:42:56 -0700

    Add styles to contributor page

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,11 +1,9 @@
 class ContributorsController < ApplicationController
   def index
-    @slug         = "everyone"
     @contributors = Contributor.all
   end
 
   def show
-    @slug        = "contributor"
     @contributor = Contributor.find_by(slug: params[:slug])
   end
 end
