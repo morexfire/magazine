5ffc30bf9974bb53a150b8fc00f85f9638c29ab5
Shane Becker <veganstraightedge@gmail.com>
2014-10-29 17:50:26 -0700

    Start a user model for auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028044839) do
+ActiveRecord::Schema.define(version: 20141028111800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,14 @@ ActiveRecord::Schema.define(version: 20141028044839) do
   create_table "roles", force: true do |t|
     t.string   "name"
     t.string   "thing"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "password_confirmatin"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
