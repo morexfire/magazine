e91edc23216b7d3d9317980822202fd5c12954af
Shane Becker <veganstraightedge@gmail.com>
2014-11-06 19:33:23 -0800

    Change feedback success notice

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,7 +3,7 @@ class FeedbacksController < ApplicationController
     @feedback = Feedback.new(feedback_params)
 
     if @feedback.save
-      redirect_to @feedback.url, notice: "Thank you for your feedback."
+      redirect_to @feedback.url, notice: "Your feedback has been received."
     else
       redirect_to @feedback.url, error:  "Sorry. It looks like something went wrong."
     end
