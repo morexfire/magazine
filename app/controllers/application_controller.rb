5913bb0ca1f0f4991a2622d0f4f8dfc739226994
Shane Becker <veganstraightedge@gmail.com>
2014-11-20 14:29:13 -0800

    Simplify #authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :current_user
   before_action :logged_in?
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
   def logged_in?
     @current_user.present?
   end
@@ -21,6 +17,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def authorize
-    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+    redirect_to root_path unless logged_in?
   end
 end
